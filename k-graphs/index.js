f60e18ea6663cfc20b9044667c33bcb3c0ac1145
kunal-mandalia <kunal.v.mandalia@gmail.com>
2017-12-30 03:31:18 +0000

    feat: compute nodes and fn on init

--- k-graphs/index.js
+++ k-graphs/index.js
@@ -1,12 +1,32 @@
+const Bijective = require('./helper/bijective')
+
 class Kgraph {
   constructor (matrix = [], labels = []) {
+    const nodes = new Set(labels)
+    const fn = new Bijective(labels)
+    this.state = {
+      matrix,
+      labels,
+      nodes,
+      fn,
+    }
+    this.expand = this.expand.bind(this)
+  }
 
+  /**
+   * Expand matrix, labels, nodes, fn
+   * @param {Array} additionalLabels 
+   */
+  expand (additionalLabels) {
+    
   }
 
   static join (graphA, graphB) {
+    const nodes = new Set(graphA.state.nodes, graphB.state.nodes)
+
+    // return new Kgraph(matrix, labels)
     return 'joining...'
   }
 }
 
 module.exports = Kgraph
-
