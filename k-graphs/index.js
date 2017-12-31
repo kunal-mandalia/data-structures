bfb1eec9952e352d205659f1fe9a6e63de60378e
kunal-mandalia <kunal.v.mandalia@gmail.com>
2017-12-31 03:07:02 +0000

    feat: add insertEmptyRowsColumns method to help expand matrix

--- k-graphs/index.js
+++ k-graphs/index.js
@@ -1,6 +1,64 @@
 const Bijective = require('./helper/bijective')
 
 class Kgraph {
+  /**
+   * 
+   * @param {Array} matrix [[], [], ...] 
+   * @param {Array} newIndices [Number]
+   * @returns {Array} matrix containing empty rows/columns at new indices
+   */
+  static insertEmptyRowsColumns (matrix, newIndices) {
+    let expandedMatrix = matrix
+    const size = matrix.length + newIndices.length
+    let emptyRow = new Array(size).fill(0)
+    // columns
+    expandedMatrix.map(row => {
+      newIndices.map(index => row.splice(index, 0, 0))
+    })
+    // rows
+    newIndices.map(index => {
+      expandedMatrix.splice(index, 0, emptyRow)
+    })
+    return expandedMatrix
+  }
+
+  /**
+   * 
+   * @param {Array} nodes 
+   */
+  static sortNodes (nodes) {
+    return nodes.sort((a,b) => a > b)
+  }
+
+  /**
+   * Expand matrix, labels, nodes, fn
+   * @param {Kgraph}
+   * @param {Array} additionalLabels 
+   * @param {Function} sortNodes
+   * @returns {Kgraph}
+   */
+  static expand (kgraph, additionalLabels, sortNodes = Kgraph.sortNodes) {
+    let matrix, labels
+    // matrix = kgraph.state.matrix
+    labels = sortNodes(Array.from(new Set(kgraph.state.labels.concat(additionalLabels))))
+    // const newIndices = labels.filter((l, i) => )
+    matrix = [
+      [0, 0, 1, 0, 1],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [1, 0, 0, 0, 0],
+    ]
+    return new Kgraph(matrix, labels)
+  }
+
+  static join (graphA, graphB) {
+    const nodes = new Set(graphA.state.nodes, graphB.state.nodes)
+    
+    // return new Kgraph(matrix, labels)
+    return 'joining...'
+  }
+
   constructor (matrix = [], labels = []) {
     const nodes = new Set(labels)
     const fn = new Bijective(labels)
@@ -10,22 +68,8 @@ class Kgraph {
       nodes,
       fn,
     }
-    this.expand = this.expand.bind(this)
-  }
-
-  /**
-   * Expand matrix, labels, nodes, fn
-   * @param {Array} additionalLabels 
-   */
-  expand (additionalLabels) {
-    
-  }
-
-  static join (graphA, graphB) {
-    const nodes = new Set(graphA.state.nodes, graphB.state.nodes)
-
-    // return new Kgraph(matrix, labels)
-    return 'joining...'
+    // this.expand = this.expand.bind(this)
+    // this.sortNodes = this.sortNodes.bind(this)
   }
 }
 
--- k-graphs/index.test.js
+++ k-graphs/index.test.js
@@ -1,8 +1,75 @@
 const Kgraph = require('./index')
+const Bijective = require('./helper/bijective')
+
+const graphA = {
+  matrix: [
+    [0, 1, 1],
+    [0, 0, 0],
+    [1, 0, 0]
+  ],
+  labels: ['a', 'b', 'c']
+}
+
+const graphB = {
+  matrix: [
+    [0, 0, 0],
+    [3, 0 ,2],
+    [5, 0, 0]
+  ],
+  labels: ['Birmingham', 'London', 'Manchester']
+}
 
 describe(`k-graph`, () => {
-  it(`should be a function`, () => {
+  it(`should be a (constructor) function`, () => {
     expect(typeof Kgraph).toEqual('function')
+  })
+
+  it(`instance should have correct state and functions on init`, () => {
+    const graph = new Kgraph(graphA.matrix, graphA.labels)
+    expect(graph.state.matrix).toEqual(graphA.matrix)
+    expect(graph.state.labels).toEqual(graphA.labels)
+    expect(graph.state.nodes).toEqual(new Set(graphA.labels))
+    expect(JSON.stringify(graph.state.fn)).toEqual(JSON.stringify(new Bijective(graphA.labels)))
+  })
+
+  describe(`static methods`, () => {
+    describe(`insertEmptyRowsColumns`, () => {
+      const newIndices = [1,3]
+      const matrix = Kgraph.insertEmptyRowsColumns(graphA.matrix, newIndices)
+      const expectedMatrix = [
+        [0, 0, 1, 0, 1],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+      ]
+      it(`should correctly expand matrix with zero valued rows/columns`, () => {
+        expect(matrix).toEqual(expectedMatrix)
+      })
+    })
+
+    describe(`expand`, () => {
+      const kGraphA = new Kgraph(graphA.matrix, graphA.labels)
+      const expandedGraph = Kgraph.expand(kGraphA, ['apple', 'banana'])
+      const labels = ['a', 'apple', 'b', 'banana', 'c']
+      const expectedState = {
+        matrix: [
+          [0, 0, 1, 0, 1],
+          [0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0],
+          [1, 0, 0, 0, 0],
+        ],
+        labels,
+        nodes: new Set(labels),
+        fn: new Bijective(labels)
+      }
+      expect(expandedGraph.state.labels).toEqual(expectedState.labels)
+    })
+
+    describe(`join`, () => {
+
+    })
   })
 
   it(`should join a pair of type 1 graphs`, () => {
@@ -35,7 +102,6 @@ describe(`k-graph`, () => {
       ],
       labels: ['a', 'b', 'Birmingham', 'c', 'London', 'Manchester']
     }
-
-    expect(Kgraph.join(graphA, graphB)).toEqual(graphC)
+    // expect(Kgraph.join(graphA, graphB)).toEqual(graphC)
   })
 })
